a0c7ceda7fec2c728528b58c927958a8a2a4640b
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-05-01 11:02:07 +0200

    changed useApi
    seems to work nicely for now, if i change method in code it can give error, but there should not be any need for that in the final version?

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-/**
- * Our API hook
- */
-function useApi(url, dataToSend = undefined, method = "GET") {
+
+function useApi(url, method = "GET") {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const options = {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  if (dataToSend) options.body = JSON.stringify(dataToSend);
-  async function getData() {
-    try {
-      setIsLoading(true);
-      setIsError(false);
-      const fetchedData = await fetch(url, options);
-      const json = await fetchedData.json();
-      console.log(json);
-      setData(json);
-    } catch (error) {
-      console.log(error);
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
+  
+const getData = async (dataToSend = undefined) => {
+    const options = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if(method !== "GET" && dataToSend){
+      options.body = JSON.stringify(dataToSend);
     }
-  }
-
-  useEffect(() => {
-    getData();
-  }, [url]);
-  const initiator = getData;
-  return { data, isLoading, isError, initiator };
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const fetchedData = await fetch(url, options);
+        const json = await fetchedData.json();
+        console.log(json);
+        setData(json);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+   if(method === "GET"){
+    useEffect(() => {
+      getData();
+    }, [url, method]);
+   }
+  
+  return { data, isLoading, isError, initiator : getData };
 }
 
 export { useApi };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,3 @@
-export const Home = () => {
-  return (
-    <div>
-   
-    </div>
-  )
+export const Home = () => {  
+  return(<div>Home</div>)
 }
