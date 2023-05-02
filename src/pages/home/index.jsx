20b90dd12ac687abd7c68a69ebda58673a8f855b
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-05-02 14:16:29 +0200

    removed test

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,6 @@ import IconSwitch from "../../components/iconSwitch"
 import PetsIcon from '@mui/icons-material/Pets';
 export const Home = () => {  
   return(<div>
-    <IconSwitch Icon={<PetsIcon/>} outlined={true}/>
   </div>)
 
 }
