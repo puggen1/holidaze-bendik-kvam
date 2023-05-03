344e99a1c73fa55d27e8790b4898b6160b4c6f09
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-05-03 09:43:26 +0200

    removed testing in home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,6 @@
 import Icons from "../../components/icons"
 
 export const Home = () => {  
-  return(<div>Home
-
-    <Icons type="card" wifi={true} parking={true} pets={true} breakfeast={true}/>
-  </div>)
+  return(<div>Home</div>)
 
 }
