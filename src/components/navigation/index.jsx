9458ac04b9f115d52ff947cdb42ce20485a9a0a1
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2023-05-08 13:11:44 +0200

    changed navigation based on logged in
    not much styling but added the content

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -5,16 +5,37 @@ import { ClearRounded, Menu } from '@mui/icons-material';
 import {NavigationDrawer, InnerDrawer} from './index.styles'
 import { Link } from 'react-router-dom';
 import {Button} from "@mui/material"
+//import button as customButton
+import CustomButton from '../Button';
 import { ModalContext } from '../../context/modalContext';
-const Navigation = () => {
+const Navigation = ({name, venueManager, logout}) => {
     const [openstatus, setOpenStatus] = useState(false);
     const { modalStatus, setModalStatus, innerContent, setInnerContent } = useContext(ModalContext)
     const change = (content) => {
-  
       if(content === ""){setOpenStatus(false); return}
       setInnerContent(content)
       setModalStatus(!modalStatus)
       setOpenStatus(false)
+    }
+    let content = (<>
+      <Button onClick={()=>{change("login")}}  sx={{color:"white", fontWeight:"300"}} variant="text">Login</Button>
+      <Button onClick={()=>{change("register")}}  sx={{color:"white", fontWeight:"300"}} variant="text">Register</Button>
+      </>)
+    
+    if(name){
+      content = (
+        <>
+        <Button onClick={()=>{change("")}} sx={{color:"white", fontWeight:"300"}} variant="text"><Link style={{ textDecoration:"none", color:"inherit"}} to="/profile">Profile</Link></Button>
+        <Button onClick={()=>{change("")}} sx={{color:"white", fontWeight:"300"}} variant="text"><Link style={{ textDecoration:"none", color:"inherit"}} to="/profile/bookings">Bookings</Link></Button>
+        {venueManager ?
+        <>
+        <Button onClick={()=>{change("")}} sx={{color:"white", fontWeight:"300"}} variant="text"><Link style={{ textDecoration:"none", color:"inherit"}} to="/profile/venues">Venues</Link></Button>
+        <Button onClick={()=>{change("")}} sx={{color:"white", fontWeight:"300"}} variant="text"><Link style={{ textDecoration:"none", color:"inherit"}} to="/admin">AdminPanel</Link></Button>
+        </>
+        : null}
+        <CustomButton variant="contained" color="error" text="Logout" event={()=>{logout();change("")}}/>
+        </>
+      )
     }
   return (
     <>
@@ -25,8 +46,7 @@ const Navigation = () => {
     <InnerDrawer>
       <ClearRounded onClick={()=>{setOpenStatus(false)}} className='iconButton' sx={{color:"white", margin:"0 0 0 auto", padding:"1rem"}} fontSize='large'/>
       <Button onClick={()=>{change("")}} sx={{color:"white", fontWeight:"300"}} variant='text'><Link style={{ textDecoration:"none", color:"inherit"}} to="/">Home</Link></Button>
-      <Button onClick={()=>{change("login")}}  sx={{color:"white", fontWeight:"300"}} variant="text">Login</Button>
-      <Button onClick={()=>{change("register")}}  sx={{color:"white", fontWeight:"300"}} variant="text">Register</Button>
+      {content}
       </InnerDrawer>
     </NavigationDrawer>
     </>
--- src/components/profile/header/index.jsx
+++ src/components/profile/header/index.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import Avatar from '../../avatar'
 import {OuterProfileHeader} from './index.styled'
+import { useContext } from 'react'
+import {ModalContext} from '../../../context/modalContext';
 const ProfileHeader = ({name, avatar}) => {
+  const {setModalStatus, setInnerContent} = useContext(ModalContext)
     //profile / use context
-    
   return (
     <OuterProfileHeader>
-        {name ?  <Link to="/profile"><Avatar type='small' username={name} src={avatar} /></Link> : <div style={{height:"50px", margin:"auto 0"}} onClick={()=>{console.log("login modal now")}}><Avatar type='small' username={name} src={avatar} /></div>}
+        {name ?  <Link to="/profile" style={{textDecoration:"none"}}><Avatar type='small' username={name} src={avatar} /></Link> : <div style={{height:"50px", margin:"auto 0"}} onClick={()=>{ setInnerContent("login"); setModalStatus(true) }}><Avatar type='small' username={name} src={avatar} /></div>}
     </OuterProfileHeader>
   )
 }
