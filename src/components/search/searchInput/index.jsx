38f931764b66bb5330064a11ad9c5e32840f1202
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2023-05-23 12:58:13 +0200

    removed controlled for now

--- src/components/search/searchInput/index.jsx
+++ src/components/search/searchInput/index.jsx
@@ -2,22 +2,14 @@ import { useContext } from 'react'
 import { SearchFilterContext } from '../../../context/searchFilterContext'
 import { Autocomplete, TextField } from '@mui/material'
 const SearchInput = () => {
-    const  {allSearchOptions, search, setSearch, searchInput, setSearchInput} = useContext(SearchFilterContext)
+    const  {allSearchOptions} = useContext(SearchFilterContext)
     
   return (
     <Autocomplete
     sx={{borderRadius:"30px"}}
     disablePortal
     id="combo-box-demo"
-    value={search}
-    onChange={(event, newValue) => {
-        setSearch(newValue);
-    }}
-    inputValue={searchInput}
-    onInputChange={(event, newInputValue) => {
-      setSearchInput(newInputValue);
-    }}
-
+    
     options={allSearchOptions}
     isOptionEqualToValue={(option, value) => option.label === value.label}
     renderOption={(props, option) => {
@@ -28,3 +20,15 @@ const SearchInput = () => {
   )
   }
 export default SearchInput
+
+/*
+value={search}
+    onChange={(event, newValue) => {
+        setSearch(newValue);
+    }}
+    inputValue={searchInput}
+    onInputChange={(event, newInputValue) => {
+      setSearchInput(newInputValue);
+
+      search, setSearch, searchInput, setSearchInput
+    }}
